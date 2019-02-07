d502c0e9dd2ad68c749926662674b58f9a4d2609
danielpriestley <dannypriestley3@gmail.com>
2019-02-07 00:30:37 +0000

    trying to fix operationalerror

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    picture = models.ImageField()
+    picture = models.ImageField( default='blog/static/media/default.jpg')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
